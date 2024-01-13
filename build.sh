58e93410451ff0335252b3c58242003f93f7a1b4
Dave Augustus <davetweetlive@gmail.com>
2024-01-13 20:26:34 +0530

    Removed unwanted files
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- build.sh
+++
@@ -1,8 +0,0 @@
-# sudo docker network create monkeys-network
-sudo docker rm -f the_monkeys_db
-sudo docker run --network=monkeys-network --name the_monkeys_db -e POSTGRES_PASSWORD=Secret -e POSTGRES_USER=root -e POSTGRES_DB=the_monkeys_user_dev -p 1234:5432 -d postgres;
-
-# Write the migration script
-
-sudo docker rm -f the_monkeys_dev
-sudo docker build -t my-golang-app . && sudo docker run --network=monkeys-network -p 8081:8081 -it --name the_monkeys_dev my-golang-app
--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd microservices/the_monkeys_gateway && CompileDaemon -build="go build -o /build/the_monkeys_gateway ." -command="/build/the_monkeys_gateway" &
-cd microservices/the_monkeys_authz && CompileDaemon -build="go build -o /build/the_monkeys_authz ." -command="/build/the_monkeys_authz" &
-wait
