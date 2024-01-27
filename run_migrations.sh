18765fcca0b57f388f0627c06ce8b6193f5a82a4
Dave Augustus <davetweetlive@gmail.com>
2024-01-27 06:35:59 +0530

    Fixed migration bug
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- run_migrations.sh
+++ run_migrations.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-for i in /scripts/*.up.sql;
+# Wait for the PostgreSQL server to start
+sleep 10
+echo "Running migrations..."
+for i in /migrations/*.up.sql
 do
-  echo Running $i;
-  PGPASSWORD=Secret psql -h the_monkeys_db -U root -d the_monkeys_user_dev -f $i;
+  echo "Running $i"
+  PGPASSWORD=Secret psql -h the_monkeys_db -U root -d the_monkeys_user_dev -f $i
 done
+echo "Migrations finished."
