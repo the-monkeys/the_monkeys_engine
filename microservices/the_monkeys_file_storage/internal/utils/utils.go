af1a6b3cca2f2f915216a1b127904010768840dd
Dave Augustus <davetweetlive@gmail.com>
2024-06-20 18:31:54 +0530

    Reset profile back to default
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- microservices/the_monkeys_file_storage/internal/utils/utils.go
+++ microservices/the_monkeys_file_storage/internal/utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"fmt"
+	"io"
+	"net/http"
 	"path/filepath"
 	"regexp"
 
@@ -22,4 +25,29 @@ func RemoveSpecialChar(val string) string {
 
 func ConstructPath(basePath, blogId, fileName string) (string, string) {
 	return filepath.Join(basePath, blogId), filepath.Join(basePath, blogId, fileName)
+}
+
+func ReadImageFromURL(url string) ([]byte, error) {
+	// Create a new HTTP client
+	client := http.Client{}
+
+	// Send a GET request to the URL
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("HTTP request failed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check for successful response status code
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("HTTP request failed with status code %d", resp.StatusCode)
+	}
+
+	// Read the response body into a byte array
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading response body: %v", err)
+	}
+
+	return data, nil
 }
