1e6135a0170affe7a48b0c27381dd88efc160634
Binary Best <mail.ppande11@gmail.com>
2024-08-26 22:31:07 +0530

    HTTP CORS fixed
    Signed-off-by: Binary Best <mail.ppande11@gmail.com>

--- microservices/the_monkeys_gateway/middleware/middleware.go
+++ microservices/the_monkeys_gateway/middleware/middleware.go
@@ -28,13 +28,17 @@ func NewCorsMiddleware() gin.HandlerFunc {
 }
 
 func CORSMiddleware() gin.HandlerFunc {
-	config := cors.Config{
-		AllowOrigins:     []string{"*"},                                                // Allow all origins
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"}, // Allow all methods
-		AllowHeaders:     []string{"*"},                                                // Allow all headers
-		AllowCredentials: true,
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
 	}
-	return cors.New(config)
 }
 
 func LogRequestBody() gin.HandlerFunc {
